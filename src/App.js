d0c9a4edba745a6a75d5ca9345411eb693598150
Nelson Wu <Nelsonwu820@gmail.com>
2023-03-18 09:45:37 -0400

    Adding the basic components in order

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Skills from "./components/Skills";
+import Project1 from "./components/Project1";
+import Project2 from "./components/Project2";
+import Project3 from "./components/Project3";
+import AboutMe from "./components/AboutMe";
+import ContactMe from "./components/ContactMe";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Header/>
-      
+      <Skills/>
+      <Project1/>
+      <Project2/>
+      <Project3/>
+      <AboutMe/>
+      <ContactMe/>
+      <Footer/>
     </div>
   );
 }

c23f1bda14bbefe518b7d23a6ed5db4bf07a9666
Nelson Wu <Nelsonwu820@gmail.com>
2023-03-20 19:21:52 -0400

    Adding table to skills with icons to display

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,23 +1,57 @@
+import { FaHtml5 } from 'react-icons/fa';
+import { IoLogoCss3 } from "react-icons/io";
+import { SiJavascript } from "react-icons/si";
+import { FaReact } from 'react-icons/fa';
+import { DiNodejs } from "react-icons/di";
+import { DiMongodb } from "react-icons/di";
+import { TbSql } from "react-icons/tb";
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+
 const Skills = () => {
   return (
     <div class="skills">
       <h1>Skills</h1>
-      <img src="" class="skills_html"/>
-      <div>HTML</div>
-      <img src="" class="skills_css"/>
-      <div>CSS</div>
-      <img src="" class="skills_js"/>
-      <div>JavaScript</div>
-      <img src="" class="skills_react"/>
-      <div>React</div>
-      <img src="" class="skills_express"/>
-      <div>Express</div>
-      <img src="" class="skills_node"/>
-      <div>Node</div>
-      <img src="" class="skills_mongodb"/>
-      <div>MongoDB</div>
-      <img src="" class="skills_sql"/>
-      <div>SQL</div>
+      <Table>
+        <Tbody>
+          <Tr>
+            <Td>
+              <FaHtml5 class="html"/>
+              <div>HTML</div>
+            </Td>
+            <Td>      
+              <IoLogoCss3/>
+              <div>CSS</div>
+            </Td>
+            <Td>
+              <SiJavascript/>
+              <div>JavaScript</div>
+            </Td>
+            <Td>
+              <FaReact/>
+              <div>React</div>
+            </Td>
+          </Tr>
+          <Tr>
+            <Td>
+              <img src="" class="skills_express"/>
+              <div>Express</div>
+            </Td>
+            <Td>
+              <DiNodejs/>
+              <div>Node</div>
+            </Td>
+            <Td>
+              <TbSql/>
+              <div>SQL</div>
+            </Td>
+            <Td>
+              <DiMongodb/>
+              <div>MongoDB</div>
+            </Td>
+          </Tr>
+        </Tbody>
+      </Table>
     </div>
   )
 }

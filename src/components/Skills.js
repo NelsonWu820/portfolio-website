bb95275c967429a23b7f06b273a364f91d1b67f9
Nelson Wu <Nelsonwu820@gmail.com>
2023-03-20 10:12:23 -0400

    Adding skills section placeholders

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,24 @@
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div class="skills">
+      <h1>Skills</h1>
+      <img src="" class="skills_html"/>
+      <div>HTML</div>
+      <img src="" class="skills_css"/>
+      <div>CSS</div>
+      <img src="" class="skills_js"/>
+      <div>JavaScript</div>
+      <img src="" class="skills_react"/>
+      <div>React</div>
+      <img src="" class="skills_express"/>
+      <div>Express</div>
+      <img src="" class="skills_node"/>
+      <div>Node</div>
+      <img src="" class="skills_mongodb"/>
+      <div>MongoDB</div>
+      <img src="" class="skills_sql"/>
+      <div>SQL</div>
+    </div>
   )
 }
 

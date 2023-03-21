7b901eaac06fd7c07f1f8770ef23b6ab33faf658
Nelson Wu <Nelsonwu820@gmail.com>
2023-03-21 16:32:13 -0400

    Changing from react responsive to flexbox

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,53 +6,44 @@ import { DiNodejs } from "react-icons/di";
 import { DiMongodb } from "react-icons/di";
 import { TbSql } from "react-icons/tb";
 import { AiFillGithub } from "react-icons/ai";
-import { Table, Tbody, Tr, Td } from 'react-super-responsive-table';
-import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 
 const Skills = () => {
   return (
-    <section class="skills" id="skills">
-      <h1>Skills & Tools</h1>
+    <section class="skills_wrapper">
+      <h1 class="skills_label">Skills & Tools</h1>
       <div class="skills w3-container">
-        <Table>
-          <Tbody>
-            <Tr>
-              <Td>
+              <div>
                 <FaHtml5 class="html"/>
                 <div>HTML</div>
-              </Td>
-              <Td>      
+              </div>
+              <div>      
                 <IoLogoCss3/>
                 <div>CSS</div>
-              </Td>
-              <Td>
+              </div>
+              <div>
                 <SiJavascript/>
                 <div>JavaScript</div>
-              </Td>
-              <Td>
+              </div>
+              <div>
                 <FaReact/>
                 <div>React</div>
-              </Td>
-            </Tr>
-            <Tr>
-              <Td>
+              </div>
+              <div>
                 <AiFillGithub/>
                 <div>GitHub</div>
-              </Td>
-              <Td>
+              </div>
+              <div>
                 <DiNodejs/>
                 <div>Node</div>
-              </Td>
-              <Td>
+              </div>
+              <div>
                 <TbSql/>
                 <div>SQL</div>
-              </Td>
-              <Td>
+              </div>
+              <div>
                 <DiMongodb/>
                 <div>MongoDB</div>
-              </Td>
-            </Tr>
-          </Tbody>
+              </div>
         </Table>
       </div>
     </section>

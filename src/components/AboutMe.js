eb5691da49b89897d420dee0711ca4f94ef84286
Nelson Wu <Nelsonwu820@gmail.com>
2023-03-22 17:37:40 -0400

    Placeholder about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,10 @@
 const AboutMe = () => {
   return (
-    <section id="aboutMe_id">AboutMe</section>
+    <section className="aboutMe">
+      <h1 className="title">About Me</h1>
+      <p className="text">I am a software developer located in New York, mainly focusing on full stack development in JavaScript using the MERN stack.
+      </p>
+    </section>
   )
 }
 

0f91d06408bc774aff0437fe4580a3da75e6db82
Nelson Wu <Nelsonwu820@gmail.com>
2023-04-06 12:47:29 -0400

    Added working links and photos to p2 desc page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Project2_desc from "./components/project_desc/Project2_desc";
 import Project3_desc from "./components/project_desc/Project3_desc";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import photo_filter from "./static/photo_filter.png"
+import todo from "./static/todo_list.png"
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
           </Route>
           <Route exact path="/Project1" element={<Project1_desc photo={photo_filter} />}>
           </Route>
-          <Route exact path="/Project2" element={<Project2_desc/>}>
+          <Route exact path="/Project2" element={<Project2_desc photo={todo} />}>
           </Route>
           <Route exact path="/Project3" element={<Project3_desc/>}>
           </Route>
--- src/components/project_desc/Project2_desc.js
+++ src/components/project_desc/Project2_desc.js
@@ -1,22 +1,38 @@
 import { Link } from "react-router-dom"
 
-const Project2_desc = () => {
+const Project2_desc = ({ photo }) => {
+  const liveUrl = "https://nelsonwu820.github.io/react-task-tracker/"
+  const sourceUrl = "https://github.com/NelsonWu820/react-task-tracker"
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-white justify-content-between fixed-top" id="navbar_parent">
-      <div class="navbar-brand text-black">
-        <Link to="/" className="project1_link">NelsonWu</Link>
+    <section>
+      <nav class="navbar navbar-expand-lg navbar-light bg-white justify-content-between fixed-top" id="navbar_parent">
+        <div class="navbar-brand text-black">
+          <Link to="/" className="project1_link">NelsonWu</Link>
+        </div>
+        
+        <div class="form-inline" data-toggle="collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <div class="nav-link text-black">
+                    <Link to="/" className="project1_link">Home</Link>
+                  </div>      
+                </li>
+            </ul>
+        </div>
+      </nav>
+
+      <div className="p2_holder">
+        <img src={photo} className="p2_img"></img> 
+        <button className="p2_btn_live" onClick={() => window.open(liveUrl)}>Live Version</button> 
+        <button className="p2_btn_source" onClick={() => window.open(sourceUrl)}>Source Code</button>
+        <h1>Description</h1>
+        <p></p>
+        <h1>Purpose</h1>
+        <p></p>
+        <h1>Problems And Thought Process</h1>
+        <p></p>
       </div>
-      
-      <div class="form-inline" data-toggle="collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <div class="nav-link text-black">
-                  <Link to="/" className="project1_link">Home</Link>
-                </div>      
-              </li>
-          </ul>
-      </div>
-    </nav>
+    </section>
   )
 }
 

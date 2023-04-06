cffee0bd254fd0903f423064288bf44ec01fa064
Nelson Wu <Nelsonwu820@gmail.com>
2023-04-06 11:58:11 -0400

    Added the project 3 placeholder

--- src/components/Project3.js
+++ src/components/Project3.js
@@ -1,17 +1,15 @@
 import { Link } from "react-router-dom"
+import In_Progress from "../static/In_Progress.jpg"
 
 const Project3 = () => {
   return (
     <section class="project1_wrapper">
       <div class="project1_img">
-        <img class="project1_img" alt="In Progress"/>
+        <img src={In_Progress} class="project1_img" alt="In Progress"/>
       </div>
       <div class="project1_text">
         <h3 class="title">In Progress</h3>
         <p>In Progress.</p>
-        <button>
-          <Link to="/Project3" className="project1_link">View It Here</Link>
-        </button>
       </div>
     </section>
   )

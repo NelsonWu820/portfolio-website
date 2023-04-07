d924b80ed4ad5e75cde002f279a8b41ec8bc0e92
Nelson Wu <Nelsonwu820@gmail.com>
2023-04-07 09:41:03 -0400

    Adds desc to individual pages

--- src/components/project_desc/Project1_desc.js
+++ src/components/project_desc/Project1_desc.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import Footer from "../Footer"
 
 const Project1_desc = ({ photo }) => {
   const liveUrl = "https://nelsonwu820.github.io/photo_filter_react/"
@@ -26,12 +27,29 @@ const Project1_desc = ({ photo }) => {
         <button className="p1_btn_live" onClick={() => window.open(liveUrl)}>Live Version</button> 
         <button className="p1_btn_source" onClick={() => window.open(sourceUrl)}>Source Code</button>
         <h1>Description</h1>
+        <p>This is a web app made Using mainly React and JavaScript with HTML and CSS. Photo Filter
+        uses a static JSON file to hold the links and tags of the images and then takes the user's
+        input to return a photos that have the tags. But it will also return photos that have all the user's
+        tags plus tags that where not part of the user's input.</p>
         <p></p>
         <h1>Purpose</h1>
+        <p>The purpose of this Photo Filter was to improve on my React abilities but have a JSON
+        file that the app can pull from. I was originally going to use the Unsplash Image API as 
+        it is a JSON API but I ran out of time and had to cut corners, so instead I have a static
+        JSON file that holds the links of the images with their tags. I do want to eventually add
+        the API to the project.</p>
         <p></p>
         <h1>Problems And Thought Process</h1>
-        <p></p>
+        <p>The main problem that I had was trying to get the relative link inside my JSON file 
+        to render the image when placed inside the img tag. As when I first just placed the relative
+        link inside the img tag it would not render the img and I had to scourer a bunch of stack overflow
+        posts about closely related topic, and asking chatGPT but not getting anywhere. Until I found one 
+        stack overflow post that was 3 years old and had 5 upvotes. Another problem I had was the logic to
+        loop through that JSON file but I was able to get over it by thinking it out and using the VScode debugging
+        panel.</p>
       </div>
+      <div className="p1_empty"></div>
+      <Footer/>
     </section>
   )
 }
--- src/components/project_desc/Project2_desc.js
+++ src/components/project_desc/Project2_desc.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import Footer from "../Footer"
 
 const Project2_desc = ({ photo }) => {
   const liveUrl = "https://nelsonwu820.github.io/react-task-tracker/"
@@ -26,12 +27,22 @@ const Project2_desc = ({ photo }) => {
         <button className="p2_btn_live" onClick={() => window.open(liveUrl)}>Live Version</button> 
         <button className="p2_btn_source" onClick={() => window.open(sourceUrl)}>Source Code</button>
         <h1>Description</h1>
+        <p>This is a pretty simple Todo List web app made from React, the web app doesn't update a database
+        like MongoDB or a JSON file yet. So any tasks that are added will only be displayed until the user
+        reloads that page, with the exception of one default task that was placed in by me.</p>
         <p></p>
         <h1>Purpose</h1>
+        <p>The main purpose of this Todo app was to test and improve my React skills as the JavaScript
+        Logic is pretty light in this app.</p>
         <p></p>
         <h1>Problems And Thought Process</h1>
-        <p></p>
+        <p>As this web app is pretty light on features the main problems I faced was just getting used 
+        to building a React app. Where I had to test my React fundamentals with stuff like useState, 
+        passing in functions from my main JavaScript file, adding logic to the JSX, etc. I was able to 
+        finish this project with the help of stack overflow and google.</p>
       </div>
+      <div className="p2_empty"></div>
+      <Footer/>
     </section>
   )
 }

34a5cd5a7cff4e0fe7a7594fe50b44ffd4dccea1
Nelson Wu <Nelsonwu820@gmail.com>
2023-08-17 11:15:27 -0400

    added photo for project 3

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Project3_desc from "./components/project_desc/Project3_desc";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import photo_filter from "./static/photo_filter.png"
 import todo from "./static/todo_list.png"
+import amazonLike from "./static/Screenshot (3).png"
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
           </Route>
           <Route exact path="/Project2" element={<Project2_desc photo={todo} />}>
           </Route>
-          <Route exact path="/Project3" element={<Project3_desc/>}>
+          <Route exact path="/Project3" element={<Project3_desc photo={amazonLike}/>}>
           </Route>
         </Routes>
       </div>

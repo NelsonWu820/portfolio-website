c26f780115ef75cf3e4b166262c2cd24d51fb5f7
Nelson Wu <Nelsonwu820@gmail.com>
2023-08-17 11:15:59 -0400

    updated about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,7 +2,7 @@ const AboutMe = () => {
   return (
     <section className="aboutMe">
       <h1 className="title">About Me</h1>
-      <p className="text">I am a software developer located in New York, mainly focusing on full stack development in JavaScript using the MERN stack.
+      <p className="text">Software developer who has been coding for over 3 years, self taught until entering college. Eager to learn more about the wide world of software development, and determined to grow as a software developer in a professional setting. Located in New York, mainly focusing on full stack development in JavaScript using the MERN stack.
       </p>
     </section>
   )

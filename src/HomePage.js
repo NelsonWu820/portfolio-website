f92f6b4026f9291a60f7ccf354fe7f3b95bb5b1f
Nelson Wu <Nelsonwu820@gmail.com>
2023-10-02 22:02:17 -0400

    added navbar right disappear when below hieght 500px

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Skills from "./components/Skills";
@@ -5,11 +6,37 @@ import ProjectTitle from "./components/ProjectTitle";
 import Project1 from "./components/Project1";
 import Project2 from "./components/Project2";
 import Project3 from "./components/Project3";
+import Project4 from './components/Project4';
 import AboutMe from "./components/AboutMe";
 import ContactMe from "./components/ContactMe";
 import Footer from "./components/Footer"; 
 
 const HomePage = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Define the threshold height at which you want to change the class
+      const thresholdHeight = 500;
+      const scrollHeight = window.scrollY;
+
+      if (scrollHeight >= thresholdHeight && !isScrolled) {
+        setIsScrolled(true);
+      } else if (scrollHeight < thresholdHeight && isScrolled) {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [isScrolled]); 
+
+  const elementClass = isScrolled ? 'navbar navbar-expand navbar-light justify-content-between fixed-top navbar-below' : 'navbar navbar-expand navbar-light bg-transparent justify-content-between fixed-top';
+
     const Scroll_Home = () => {
         const element = document.getElementById("header_id");
         if (element) {
@@ -46,14 +73,15 @@ const HomePage = () => {
         }
       };
   return (
-    <div>
+    <div className='homepage-holder'>
         <Header/>
-        <Navbar onClick={Scroll_Home} onClick2={Scroll_Skills} onClick3={Scroll_ProjectTitle} onClick4={Scroll_AboutMe} onClick5={Scroll_ContactMe}/>
+        <Navbar name={elementClass} onClick={Scroll_Home} onClick2={Scroll_Skills} onClick3={Scroll_ProjectTitle} onClick4={Scroll_AboutMe} onClick5={Scroll_ContactMe}/>
         <Skills/>
         <ProjectTitle/>
         <Project1/>
         <Project2/>
         <Project3/>
+        <Project4/>
         <AboutMe/>
         <ContactMe/>
         <Footer/>

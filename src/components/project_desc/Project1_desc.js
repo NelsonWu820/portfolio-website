3de333def6e620879242a157e6d513f75a4b2fae
Nelson Wu <Nelsonwu820@gmail.com>
2023-10-02 22:02:59 -0400

    changed desc and btn stlying plus class to className

--- src/components/project_desc/Project1_desc.js
+++ src/components/project_desc/Project1_desc.js
@@ -1,56 +1,64 @@
-import { Link } from "react-router-dom"
-import Footer from "../Footer"
-import { useEffect } from "react"
+import { Link } from "react-router-dom";
+import Footer from "../Footer";
+import { useEffect } from "react";
+import {MdOutlineLiveTv} from "react-icons/md";
+import {FaGithub} from "react-icons/fa";
 
 const Project1_desc = ({ photo }) => {
   useEffect(() => {window.scroll(0,0)});
-  const liveUrl = "https://nelsonwu820.github.io/photo_filter_react/"
-  const sourceUrl = "https://github.com/NelsonWu820/photo_filter_react"
+  const liveUrl = "https://amazonlike.onrender.com"
+  const sourceUrl = 'https://github.com/NelsonWu820/amazonLike'
   return (
-    <section>
-      <nav class="navbar navbar-expand-lg navbar-light bg-white justify-content-between fixed-top" id="navbar_parent">
-        <div class="navbar-brand text-black">
+    <section className="p3_desc">
+      <nav className="navbar navbar-expand-lg navbar-light bg-transparent justify-content-between fixed-top" id="navbar_parent">
+        <div className="navbar-brand text-black">
           <Link to="/" className="project1_link">NelsonWu</Link>
         </div>
         
-        <div class="form-inline" data-toggle="collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                  <div class="nav-link text-black">
+        <div className="form-inline" data-toggle="collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <div className="nav-link text-black">
                     <Link to="/" className="project1_link">Home</Link>
                   </div>      
                 </li>
             </ul>
         </div>
       </nav>
-
-      <div className="p1_holder">
-        <img src={photo} className="p1_img"></img> 
-        <button className="p1_btn_live" onClick={() => window.open(liveUrl)}>Live Version</button> 
-        <button className="p1_btn_source" onClick={() => window.open(sourceUrl)}>Source Code</button>
-        <h1>Description</h1>
-        <p>This is a web app made Using mainly React and JavaScript with HTML and CSS. Photo Filter
-        uses a static JSON file to hold the links and tags of the images and then takes the user's
-        input to return a photos that have the tags. But it will also return photos that have all the user's
-        tags plus tags that where not part of the user's input.</p>
-        <p></p>
-        <h1>Purpose</h1>
-        <p>The purpose of this Photo Filter was to improve on my React abilities but have a JSON
-        file that the app can pull from. I was originally going to use the Unsplash Image API as 
-        it is a JSON API but I ran out of time and had to cut corners, so instead I have a static
-        JSON file that holds the links of the images with their tags. I do want to eventually add
-        the API to the project.</p>
-        <p></p>
-        <h1>Problems And Thought Process</h1>
-        <p>The main problem that I had was trying to get the relative link inside my JSON file 
-        to render the image when placed inside the img tag. As when I first just placed the relative
-        link inside the img tag it would not render the img and I had to scourer a bunch of stack overflow
-        posts about closely related topic, and asking chatGPT but not getting anywhere. Until I found one 
-        stack overflow post that was 3 years old and had 5 upvotes. Another problem I had was the logic to
-        loop through that JSON file but I was able to get over it by thinking it out and using the VScode debugging
-        panel.</p>
+  
+      <div className="p_holder">
+      <img src={photo} className="p_img" alt=".../"></img> 
+      <div className="p1_btn_live">
+        <button className="button-31"onClick={() => window.open(liveUrl)}> <MdOutlineLiveTv className="live_icon"/>Live Version (It might take a while, as I deployed it using a free plan on Render)</button> 
       </div>
-      <div className="p1_empty"></div>
+      <div className="p1_btn_source">
+        <button className="button-31" onClick={() => window.open(sourceUrl)}> <FaGithub className="source_icon"/>Source Code</button>
+      </div>        
+      <h1>Description</h1>
+      <p>This is my most complex web app to date. This web application is made using MERN (MongoDB, Express.js, React, Node.js) where MongoDB 
+      is the database, Express.js is a framework that runs on Node.js that makes it much easier to write API's. While React is used for the frontend with Redux to
+      manage global states, and Node.js is used for the server side. As the name suggests this will be an Amazon clone where users will be able
+      to add items to cart then purchase from there, as well as leave comments and rating on items. Where the Fake Store API will be used to fill up the items in the store.
+      </p>
+      <p></p>
+      <h1>Purpose</h1>
+      <p>The main purpose of this Amazon clone is to test my MERN skills and prove that I can work with a full stack application. 
+      That uses a dynamic frontend to talk to a Node backend, which will actually store data to be used later. Which will also
+      include the use of an API (Fake Store API) to fill in some of the products.
+      </p>
+      <p></p>
+      <h1>Problems And Thought Process</h1>
+      <p>
+        The general idea was preety clear as I wanted to build an Amazon clone so I just looked at what amazon was doing. The problem
+        with that, is that Amazon is a huge company I quickly realized how big this project was going to be if I wanted to add 
+        every feature Amazon had. So I quickly had to cut a lot of features as I did not have the time to make the perfect web application,
+        so I just stuck to the basics of buy and rating items. At first I wanted to fill the store using Amazon's Product Advertesting API
+        but the thing is to get access to the API I would have to become an Amazon affliate and actually sell items, then go through a
+        review and even if I got through the review somehow I would have periodically give Amazon evidence I'm actually selling stuff. 
+        But I just wanted to test my skills so I just opted for the much simpler Fake Store API.
+      </p>
+      </div>
+      <div className="p3_empty"></div>
       <Footer/>
     </section>
   )
--- src/components/project_desc/Project2_desc.js
+++ src/components/project_desc/Project2_desc.js
@@ -1,49 +1,65 @@
 import { Link } from "react-router-dom"
 import Footer from "../Footer"
 import { useEffect } from "react";
+import {MdOutlineLiveTv} from "react-icons/md";
+import {FaGithub} from "react-icons/fa";
 
 const Project2_desc = ({ photo }) => {
   useEffect(() => {window.scroll(0,0)});
-  const liveUrl = "https://nelsonwu820.github.io/react-task-tracker/"
-  const sourceUrl = "https://github.com/NelsonWu820/react-task-tracker"
+  const liveUrl = "https://developernetwork.onrender.com"
+  const sourceUrl = 'https://github.com/NelsonWu820/DeveloperNetwork'
   return (
-    <section>
-      <nav class="navbar navbar-expand-lg navbar-light bg-white justify-content-between fixed-top" id="navbar_parent">
-        <div class="navbar-brand text-black">
+    <section className="p3_desc">
+      <nav className="navbar navbar-expand-lg navbar-light bg-transparent justify-content-between fixed-top" id="navbar_parent">
+        <div className="navbar-brand text-black">
           <Link to="/" className="project1_link">NelsonWu</Link>
         </div>
         
-        <div class="form-inline" data-toggle="collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                  <div class="nav-link text-black">
+        <div className="form-inline" data-toggle="collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <div className="nav-link text-black">
                     <Link to="/" className="project1_link">Home</Link>
                   </div>      
                 </li>
             </ul>
         </div>
       </nav>
-
-      <div className="p2_holder">
-        <img src={photo} className="p2_img"></img> 
-        <button className="p2_btn_live" onClick={() => window.open(liveUrl)}>Live Version</button> 
-        <button className="p2_btn_source" onClick={() => window.open(sourceUrl)}>Source Code</button>
-        <h1>Description</h1>
-        <p>This is a pretty simple Todo List web app made from React, the web app doesn't update a database
-        like MongoDB or a JSON file yet. So any tasks that are added will only be displayed until the user
-        reloads that page, with the exception of one default task that was placed in by me.</p>
-        <p></p>
-        <h1>Purpose</h1>
-        <p>The main purpose of this Todo app was to test and improve my React skills as the JavaScript
-        Logic is pretty light in this app.</p>
-        <p></p>
-        <h1>Problems And Thought Process</h1>
-        <p>As this web app is pretty light on features the main problems I faced was just getting used 
-        to building a React app. Where I had to test my React fundamentals with stuff like useState, 
-        passing in functions from my main JavaScript file, adding logic to the JSX, etc. I was able to 
-        finish this project with the help of stack overflow and google.</p>
+  
+      <div className="p_holder">
+      <img src={photo} className="p_img" alt=".../"></img> 
+      <div className="p1_btn_live">
+        <button className="button-31"onClick={() => window.open(liveUrl)}> <MdOutlineLiveTv className="live_icon"/>Live Version (It might take a while, as I deployed it using a free plan on Render)</button> 
       </div>
-      <div className="p2_empty"></div>
+      <div className="p1_btn_source">
+        <button className="button-31" onClick={() => window.open(sourceUrl)}> <FaGithub className="source_icon"/>Source Code</button>
+      </div>          
+      <h1>Description</h1>
+      <p>This web application is made using MERN (MongoDB, Express.js, React, Node.js) where the frontend will
+      be made using HTML, CSS and React with Redux to manage states. The backend will use Nodejs with Express.js, 
+      and uses mongoose to interacte with MongoDB Atlas. The app is meant to mimic a basic social media app, where users
+      can make a post to a general chat that can be commented on by other users, and liked. Each user can have a profile that has their
+      basic information, education, skills, experiences, etc. Users can also link social media accounts as well as github repos which can
+      be seen when another user looks at their profile.
+      </p>
+      <p></p>
+      <h1>Purpose</h1>
+      <p>The main purpose of this Developer Network was to test my skills in building web applications using the MERN stack. As well as to
+        try at building a web application where users can interacte with other users. As well as to see how the github access token works
+        in live applications. 
+      </p>
+      <p></p>
+      <h1>Problems And Thought Process</h1>
+      <p>
+        This is the first time that I have worked with a database within a full stack web app,
+        also as I have never used a document based database it took me a while to get used to MongoDB coming 
+        from a SQL background. So when building my data models for the web application I tried to make them
+        relation before realizing that I had to learn how to properly store data in a document database.
+        Also the use of redux and global states was a unqiue add on to base React, but it allowed me 
+        to do a lot with affecting multiple compoenents at once. 
+      </p>
+      </div>
+      <div className="p3_empty"></div>
       <Footer/>
     </section>
   )
